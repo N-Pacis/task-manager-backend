b8a7785ea396334123264fb83800028f0232a6cc
nkubito-pacis <74002896+N-Pacis@users.noreply.github.com>
2024-01-29 18:04:38 +0200

    Dockerized the application

--- database/config.cjs
+++ database/config.cjs
@@ -1,18 +1,20 @@
 require('dotenv').config();
 
 module.exports = {
-  test: {
+  TEST: {
     username: process.env.TEST_DB_USER,
     password: process.env.TEST_DB_PASSWORD,
     database: process.env.TEST_DB_NAME,
     host: process.env.TEST_DB_HOST,
+    port: process.env.TEST_DB_PORT,
     dialect: process.env.DB_DIALECT,
   },
-  development: {
+  DEV: {
     username: process.env.DEV_DB_USER,
     password: process.env.DEV_DB_PASSWORD,
     database: process.env.DEV_DB_NAME,
     host: process.env.DEV_DB_HOST,
+    port: process.env.DEV_DB_PORT,
     dialect: process.env.DB_DIALECT
   },
   actions: {
@@ -20,13 +22,15 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: process.env.DB_DIALECT,
   },
-  production: {
+  PROD: {
     username: process.env.PROD_DB_USER,
     password: process.env.PROD_DB_PASSWORD,
     database: process.env.PROD_DB_NAME,
     host: process.env.PROD_DB_HOST,
+    port: process.env.PROD_DB_PORT,
     dialect: process.env.DB_DIALECT
   }
 };
